9f6444b423e555f8141b38a3feee102d13b62c00
eerotpt <eero.sakson@tptlive.ee>
2017-11-02 09:38:33 +0200

    Objektid c#

---
+++ objektid/objektid/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objektid
+{
+    class Cat
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Cat()
+        {
+            Name = "Kiisu";
+            Age =3;
+            Color = "Brown";
+            
+        }
+
+        public void Miau()
+        {
+            Console.WriteLine("Miäu miäu!");
+        }
+
+    }
+}
---
+++ objektid/objektid/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objektid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+            dog1.SetAge = (7);
+            dog1.Name = "Muki";
+            dog1.Color = "Yellow";
+
+            Console.WriteLine("Koera nimi: " + dog1.Name);
+            Console.WriteLine("Koera värv: " + dog1.Color);
+            Console.WriteLine("Koera vanus: " + dog1.Age);
+            Console.WriteLine("");
+
+            //Dog dog2 = new Dog();
+            //Console.WriteLine("Sisesta koera nimi: ");
+            //dog2.Name = Console.ReadLine();
+            //Console.WriteLine("Sisesta koera värv: ");
+            //dog2.Color = Console.ReadLine();
+            //Console.WriteLine("Sisesta koera vanus: ");
+            //dog2.Age = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Koera nimi: " + dog2.Name);
+            //Console.WriteLine("Koera värv: " + dog2.Color);
+            //Console.WriteLine("Koera vanus: " + dog2.Age);
+
+            Dog dog3 = new Dog();
+            Console.WriteLine(dog3.Name);
+            Console.WriteLine(dog3.Age);
+            Console.WriteLine(dog3.Color);
+            dog3.Bark();
+            Console.WriteLine("Koeara tegelik vanus on: " + dog3.CalculateDogYears());
+            Console.WriteLine("Koeral on " + dog3.NumberOfEyes + " silma");
+            Console.WriteLine(dog3.IsCarnivore);
+            Console.WriteLine("");
+
+            Cat cat1 = new Cat();
+            Console.WriteLine("Kassi nimi on: " + cat1.Name);
+            Console.WriteLine("Kassi vanus on: " + cat1.Age);
+            Console.WriteLine("Kassi värv on: " + cat1.Color);
+            cat1.Miau();
+            Console.WriteLine("");
+
+            Cat cat2 = new Cat();
+            Console.WriteLine("Sisesta kassi nimi: ");
+            cat2.Name = Console.ReadLine();
+            Console.WriteLine("Sisesta kassi vanus: ");
+            cat2.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta kassi värv: ");
+            cat2.Color = Console.ReadLine();
+
+            Console.WriteLine("Kassi nimi: " + cat2.Name);
+            Console.WriteLine("Kassi vanus: " + cat2.Age);
+            Console.WriteLine("Kassi värv: " + cat2.Color);
+            Console.WriteLine("");
+
+            Mammal mammal1 = new Mammal();
+
+
+            Console.ReadLine();
+        }
+    }
+}
